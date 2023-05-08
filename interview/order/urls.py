d4bc80ea008ace1fe4ecef6b3e93e2cfdbfb3ba8
galarant <jeffrey.revesz@gmail.com>
2023-05-08 13:15:47 -0600

    Added edge endpoint for OrderTags

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrderTagsEdgeView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
+    path('<int:order_id>/tags/', OrderTagsEdgeView.as_view(), name='order-tags-edge'),
 
 ]

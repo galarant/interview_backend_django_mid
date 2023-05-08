adadc192852b32acdf40ef4b2cabdc220519229f
galarant <jeffrey.revesz@gmail.com>
2023-05-08 13:01:29 -0600

    created DeactivateOrderView

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +16,18 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class DeactivateOrderView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs['id'])
+        order.is_active = False
+        serializer = self.serializer_class(order)
+
+        return Response(serializer.data, status=200)
+
+    def get_queryset(self, **kwargs):
+        return self.queryset.get(**kwargs)
